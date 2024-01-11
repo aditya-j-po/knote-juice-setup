4c827ef918be2b11b07caec3c883ce15a0e5548e
Aditya <aditya.j@protectonce.com>
2024-01-11 16:17:14 +0530

    setup demo app

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+# Stop and delete the Minikube cluster
+minikube stop
+minikube delete
+
+# Remove the Minikube VM
+minikube delete --all --purge
---
+++ create.sh
@@ -0,0 +1,4 @@
+minikube start
+kubectl apply -f ./yamls
+minikube service --all
+# minikube dashboard --port 5004
